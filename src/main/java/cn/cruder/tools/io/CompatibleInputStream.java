82671be8205c184ec1035d1d78fe055049181103
DouShaoxun <dousx_cn@163.com>
2022-05-08 18:19:55 +0800

    CompatibleInputStream

---
+++ src/main/java/cn/cruder/tools/io/CompatibleInputStream.java
@@ -0,0 +1,52 @@
+package cn.cruder.tools.io;
+
+import java.io.*;
+
+/**
+ * 解决反序列化版本不一致问题
+ *
+ * @author dousx
+ * @date 2022-05-08 18:17
+ */
+public class CompatibleInputStream extends ObjectInputStream {
+    public CompatibleInputStream(InputStream in) throws IOException {
+        super(in);
+    }
+
+    @Override
+    protected ObjectStreamClass readClassDescriptor() throws ClassNotFoundException, IOException {
+        ObjectStreamClass resultClassDescriptor = null;
+        Class localClass = null;
+
+        try {
+            resultClassDescriptor = super.readClassDescriptor();
+            localClass = Class.forName(resultClassDescriptor.getName());
+            ObjectStreamClass localClassDescriptor = ObjectStreamClass.lookup(localClass);
+            if (localClassDescriptor != null) {
+                long localSUID = localClassDescriptor.getSerialVersionUID();
+                long streamSUID = resultClassDescriptor.getSerialVersionUID();
+                if (streamSUID != localSUID) {
+                    StringBuffer buffer = new StringBuffer("Overriding serialized class version mismatch: ");
+                    buffer.append("local serialVersionUID = ").append(localSUID);
+                    buffer.append(" stream serialVersionUID = ").append(streamSUID);
+                    resultClassDescriptor = localClassDescriptor;
+                }
+            }
+
+            return resultClassDescriptor;
+        } catch (ClassNotFoundException | IOException var9) {
+            throw var9;
+        }
+    }
+
+    /**
+     * 修改byte数组中对象的VersionId，返回对象
+     *
+     * @return {@link Object}
+     */
+    public static Object byteToObject(byte[] oldBytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream tokenByteArrayInputStream = new ByteArrayInputStream(oldBytes);
+        CompatibleInputStream tokenInputStream = new CompatibleInputStream(tokenByteArrayInputStream);
+        return tokenInputStream.readObject();
+    }
+}
